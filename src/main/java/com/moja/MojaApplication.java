fb44212102e48bb607d096e472ddb23aeffc28c9
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2024-12-01 20:38:46 +0530

    Files added

---
+++ src/main/java/com/moja/MojaApplication.java
@@ -0,0 +1,13 @@
+package com.moja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojaApplication.class, args);
+	}
+
+}
